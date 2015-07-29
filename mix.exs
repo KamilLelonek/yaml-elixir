19c1a8d5188275dea79312d070feee7b469ad86f
Kamil Lelonek <squixy.sln@gmail.com>
2015-07-29 15:10:38 +0200

    Bumped hex version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule YamlElixir.Mixfile do
   def project do
     [
       app:             :yaml_elixir,
-      version:         "0.0.1",
+      version:         "1.0.0",
       elixir:          "~> 1.0",
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
