5616fea744d0808e1e14231f8d7789de4b5d553e
Ernie Miller <ernie@erniemiller.org>
2015-09-20 10:55:49 -0400

    Allow atom keys or values in maps with atoms: true
    I greatly missed this feature (enabled by default) in typical Ruby YAML
    parsing. However, since atoms aren't garbage collected, it's not really
    safe to enable this by default.
    
    Additionally, `yamerl` can output binaries for us, so we don't need to
    do the `to_string` as we were.

--- lib/yaml_elixir.ex
+++ lib/yaml_elixir.ex
@@ -3,32 +3,37 @@ defmodule YamlElixir do
     Supervisor.start_link(children, options)
   end
 
+  @yamerl_options [
+    detailed_constr: true,
+    str_node_as_binary: true,
+  ]
+
   defp children, do: []
   defp options,  do: [strategy: :one_for_one, name: YamlElixir.Supervisor]
 
-  def read_all_from_file(path) do
+  def read_all_from_file(path, options \\ []) do
     path
-      |> :yamerl_constr.file(detailed_constr: true)
-      |> YamlElixir.Mapper.process
+      |> :yamerl_constr.file(@yamerl_options)
+      |> YamlElixir.Mapper.process(options)
   end
 
-  def read_from_file(path) do
+  def read_from_file(path, options \\ []) do
     path
-      |> :yamerl_constr.file(detailed_constr: true)
+      |> :yamerl_constr.file(@yamerl_options)
       |> List.last
-      |> YamlElixir.Mapper.process
+      |> YamlElixir.Mapper.process(options)
   end
 
-  def read_all_from_string(string) do
+  def read_all_from_string(string, options \\ []) do
     string
-      |> :yamerl_constr.string(detailed_constr: true)
-      |> YamlElixir.Mapper.process
+      |> :yamerl_constr.string(@yamerl_options)
+      |> YamlElixir.Mapper.process(options)
   end
 
-  def read_from_string(string) do
+  def read_from_string(string, options \\ []) do
     string
-    |> :yamerl_constr.string(detailed_constr: true)
+    |> :yamerl_constr.string(@yamerl_options)
     |> List.last
-    |> YamlElixir.Mapper.process
+    |> YamlElixir.Mapper.process(options)
   end
 end
