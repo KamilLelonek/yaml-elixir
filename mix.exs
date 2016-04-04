c37aa3da3189d2b3ff204ecd732746801573c8f5
Kamil Lelonek <squixy.sln@gmail.com>
2016-04-04 09:46:49 +0800

    Change contributors to maintainers

--- mix.exs
+++ mix.exs
@@ -41,10 +41,10 @@ defmodule YamlElixir.Mixfile do
 
   defp package do
     [
-      files:        ["lib", "config", "mix.exs", "README.md"],
-      contributors: ["Kamil Lelonek"],
-      licenses:     ["MIT"],
-      links:        %{ "GitHub" => "https://github.com/KamilLelonek/yaml-elixir" }
+      files:       ["lib", "config", "mix.exs", "README.md"],
+      maintainers: ["Kamil Lelonek"],
+      licenses:    ["MIT"],
+      links:       %{ "GitHub" => "https://github.com/KamilLelonek/yaml-elixir" }
     ]
   end
 end
