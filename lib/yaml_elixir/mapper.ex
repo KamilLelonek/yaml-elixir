bdfd317a7383332b470cad37d924b636a5564222
Haito <hajtosek@gmail.com>
2018-05-31 20:05:55 +0200

    Allow maps as keywords

--- lib/yaml_elixir/mapper.ex
+++ lib/yaml_elixir/mapper.ex
@@ -1,15 +1,15 @@
 defmodule YamlElixir.Mapper do
-  def process(nil, _options), do: %{}
+  def process(nil, options), do: empty_container(options)
   def process(yaml, options) when is_list(yaml), do: Enum.map(yaml, &process(&1, options))
 
   def process(yaml, options),
     do:
       yaml
       |> _to_map(options)
-      |> extract_map()
+      |> extract_map(options)
 
-  defp extract_map(nil), do: %{}
-  defp extract_map(map), do: map
+  defp extract_map(nil, options), do: empty_container(options)
+  defp extract_map(map, _), do: map
 
   defp _to_map({:yamerl_doc, document}, options), do: _to_map(document, options)
 
@@ -17,7 +17,7 @@ defmodule YamlElixir.Mapper do
     do: Enum.map(seq, &_to_map(&1, options))
 
   defp _to_map({:yamerl_map, :yamerl_node_map, _tag, _loc, map_tuples}, options),
-    do: _tuples_to_map(map_tuples, %{}, options)
+    do: _tuples_to_map(map_tuples, empty_container(options), options)
 
   defp _to_map(
          {:yamerl_str, :yamerl_node_str, _tag, _loc, <<?:, elem::binary>> = original_elem},
@@ -36,18 +36,24 @@ defmodule YamlElixir.Mapper do
   defp _tuples_to_map([], map, _options), do: map
 
   defp _tuples_to_map([{key, val} | rest], map, options) do
+    agregator_module =
+      case Keyword.get(options, :maps_as_keywords) do
+        true -> &append_kv/3
+        _ -> &Map.put_new/3
+      end
+
     case key do
       {:yamerl_seq, :yamerl_node_seq, _tag, _log, _seq, _n} ->
         _tuples_to_map(
           rest,
-          Map.put_new(map, _to_map(key, options), _to_map(val, options)),
+          agregator_module.(map, _to_map(key, options), _to_map(val, options)),
           options
         )
 
       {_yamler_element, _yamler_node_element, _tag, _log, name} ->
         _tuples_to_map(
           rest,
-          Map.put_new(map, key_for(name, options), _to_map(val, options)),
+          agregator_module.(map, key_for(name, options), _to_map(val, options)),
           options
         )
     end
@@ -62,4 +68,15 @@ defmodule YamlElixir.Mapper do
   end
 
   defp key_for(name, _options), do: name
+
+  defp empty_container(options) do
+    case Keyword.get(options, :maps_as_keywords) do
+      true -> []
+      _ -> %{}
+    end
+  end
+
+  defp append_kv(list, key, value) do
+    [{key, value} | list]
+  end
 end
--- test/yaml_elixir_test.exs
+++ test/yaml_elixir_test.exs
@@ -168,19 +168,13 @@ defmodule YamlElixirTest do
     assert {:error, "malformed yaml"} = YamlElixir.read_from_file(path)
   end
 
-  # test "should receive keyword list when used `maps_as_keywords` option" do
-  #   assert_parse_file(
-  #     "nested",
-  #     [
-  #       [
-  #         {"prod", [{"foo", "foo"}]},
-  #         {"dev", [{"foo", "bar"}]},
-  #         {"test", [{"foo", "baz"}]}
-  #       ]
-  #     ],
-  #     maps_as_keywords: true
-  #   )
-  # end
+  test "should receive keyword list when used `maps_as_keywords` option" do
+    assert_parse_file(
+      "nested",
+      [[[{"prod", [{"foo", "foo"}]}, {"dev", [{"foo", "bar"}]}, {"test", [{"foo", "baz"}]}]]],
+      maps_as_keywords: true
+    )
+  end
 
   defp test_data(file_name), do: Path.join(File.cwd!(), "test/fixtures/#{file_name}.yml")
 
