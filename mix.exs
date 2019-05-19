2a24c4e4840a1419359f4c5288c19186db2a8cae
Kamil Lelonek <squixy.sln@gmail.com>
2019-05-19 20:33:43 +0200

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule YamlElixir.Mixfile do
   def project do
     [
       app: :yaml_elixir,
-      version: "2.3.0",
+      version: "2.4.0",
       elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
